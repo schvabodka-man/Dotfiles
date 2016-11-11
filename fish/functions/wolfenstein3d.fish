2487889945e3c152d9d112c7ee5991ca66aedd52
schvabodka-man <scvhapps@gmail.com>
2016-11-11 15:19:29 +0200

    wolf3d config

--- fish/functions/wolfenstein3d.fish
+++ fish/functions/wolfenstein3d.fish
@@ -3,5 +3,6 @@ function wolfenstein3d
   set bwpath '/home/scvh/Games/wad/brutralwolf3d/BW-V4.5.pk3'
   set bwmusic '/home/scvh/Games/wad/brutralwolf3d/BW-MusicPack.wad'
   set bwhud '/home/scvh/Games/wad/brutralwolf3d/BW-HUDPATCH.pk3'
-  gzdoom -file $bwpath $bwmusic $bwhud
+  set config '/home/scvh/.config/gzdoom/wolf3d.ini'
+  gzdoom -config $config -file $bwpath $bwmusic $bwhud
 end
--- fish/shortcuts/wolfenstein3d_for_desktop.fish
+++ fish/shortcuts/wolfenstein3d_for_desktop.fish
@@ -2,4 +2,5 @@
 set bwpath '/home/scvh/Games/wad/brutralwolf3d/BW-V4.5.pk3'
 set bwmusic '/home/scvh/Games/wad/brutralwolf3d/BW-MusicPack.wad'
 set bwhud '/home/scvh/Games/wad/brutralwolf3d/BW-HUDPATCH.pk3'
-gzdoom -file $bwpath $bwmusic $bwhud
+set config '/home/scvh/.config/gzdoom/wolf3d.ini'
+gzdoom -config $config -file $bwpath $bwmusic $bwhud
