c105772d2f926a16207f7b7ac282bdd52644f5a6
schvabodka-man <scvhapps@gmail.com>
2016-11-11 15:23:29 +0200

    wolf3d config

--- fish/functions/wolfenstein3d.fish
+++ fish/functions/wolfenstein3d.fish
@@ -1,8 +1,5 @@
 #Well, there are some interesting stuff so i got to make two seperate scipts - one for desktop launcher and second for terminal
 function wolfenstein3d
-  set bwpath '/home/scvh/Games/wad/brutralwolf3d/BW-V4.5.pk3'
-  set bwmusic '/home/scvh/Games/wad/brutralwolf3d/BW-MusicPack.wad'
-  set bwhud '/home/scvh/Games/wad/brutralwolf3d/BW-HUDPATCH.pk3'
   set config '/home/scvh/.config/gzdoom/wolf3d.ini'
-  gzdoom -config $config -file $bwpath $bwmusic $bwhud
+  gzdoom -config $config
 end
--- fish/shortcuts/wolfenstein3d_for_desktop.fish
+++ fish/shortcuts/wolfenstein3d_for_desktop.fish
@@ -1,6 +1,3 @@
 #!/usr/bin/fish
-set bwpath '/home/scvh/Games/wad/brutralwolf3d/BW-V4.5.pk3'
-set bwmusic '/home/scvh/Games/wad/brutralwolf3d/BW-MusicPack.wad'
-set bwhud '/home/scvh/Games/wad/brutralwolf3d/BW-HUDPATCH.pk3'
 set config '/home/scvh/.config/gzdoom/wolf3d.ini'
-gzdoom -config $config -file $bwpath $bwmusic $bwhud
+gzdoom -config $config
