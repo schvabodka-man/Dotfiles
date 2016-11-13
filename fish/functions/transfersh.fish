32d7c5de75479f58544dc3aef32b72771f099149
schvabodka-man <scvhapps@gmail.com>
2016-11-13 21:02:38 +0200

    Finally, transfersh api

--- fish/functions/transfersh.fish
+++ fish/functions/transfersh.fish
@@ -1,5 +1,8 @@
 function transfersh
+  set icon '/home/scvh/Pictures/icons/upload.png'
   for file in $argv
-    curl --upload-file $file https://transfer.sh/$file 
+    curl --upload-file $file https://transfer.sh/$file | xclip -selection clipboard
+    notify-send -i $icon "Uploading $file"
   end
+  notify-send -i $icon "All files uploaded"
 end
