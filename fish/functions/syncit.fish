d368f51ba0a95b0e847ea109dd8003b91e2ee028
schvabodka-man <scvhapps@gmail.com>
2016-11-14 08:29:42 +0200

    fancy stuff

--- fish/functions/syncit.fish
+++ fish/functions/syncit.fish
@@ -1,3 +1,8 @@
 function syncit
-  cp $argv /home/scvh/Sync
+  if cp $argv /home/scvh/Sync > /dev/null
+    notify-send -i /home/scvh/Pictures/icons/sync.png "Synced!"
+    echo "Synced files $argv"
+  else
+    notify-send -i /home/scvh/Pictures/icons/error.png "Error in sync"
+  end
 end
--- fish/panel/deadbeef_current_song.fish
+++ fish/panel/deadbeef_current_song.fish
@@ -1,5 +1,3 @@
 #!/usr/bin/fish
-if pgrep 'deadbeef' > /dev/null
-  set deadbeef_song (deadbeef --nowplaying-tf "%artist% - %title%")
-  echo ♬: $deadbeef_song
-end
+set deadbeef_song (deadbeef --nowplaying-tf "%artist% - %title%")
+echo ♬: $deadbeef_song
