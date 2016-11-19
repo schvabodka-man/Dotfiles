25def4858a8f805e6d632e66f54f466115c7f1a8
schvabodka-man <scvhapps@gmail.com>
2016-11-19 20:25:39 +0200

    coloring help script

--- fish/functions/help.fish
+++ fish/functions/help.fish
@@ -1,17 +1,28 @@
 function help
+    set_color yellow
     echo "Help"
+    set_color green
     echo "File Managing:"
+    set_color normal
     echo "browser - run thunar as super user"
     echo "cdls - run ls after cd"
+    set_color green
     echo "Syncing:"
+    set_color normal
     echo "transfersh - upload file(s) to transfer.sh"
     echo "syncit - sync file with my other devices"
+    set_color green
     echo "Google Drive:"
+    set_color normal
     echo "gdrive-add - add new gdrive to pull(first argument - name, second - api token)"
     echo "gdrive-mount - mount current gdrive's to file system"
+    set_color green
     echo "System:"
+    set_color normal
     echo "rofi-cache-clear - clear rofi cache"
     echo "xresourcesupdate - update .xresources database"
+    set_color green
     echo "Games:"
+    set_color normal
     echo "wolfenstein3d - run brutal wolfenstein"
 end
