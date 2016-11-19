2f31d132e7c4f6fc1b100d67ac9e1b4bf9ee3251
schvabodka-man <scvhapps@gmail.com>
2016-11-19 20:14:43 +0200

    adding new gdrive discs to pull

---
+++ fish/functions/gdrive-add.fish
@@ -0,0 +1,7 @@
+#adding a new gdrive by creds and name, first name than creds
+function gdrive-add
+  cd /home/scvh/gdrive/
+  mkdir $argv[1]
+  cd gdrivekeys
+  gdfstool auth -a /home/scvh/gdrive/gdrivekeys/$argv[1] "$argv[2]"
+end
