9a5d34b5381b98abccc79a74d93d6b22c576a437
schvabodka-man <scvhapps@gmail.com>
2016-12-03 16:41:54 +0200

    tasker profile for wifi

--- fish/functions/syncit.fish
+++
@@ -1,8 +0,0 @@
-function syncit
-  if cp $argv /home/scvh/Sync > /dev/null
-    notify-send -i /home/scvh/Pictures/icons/sync.png "Synced!"
-    echo "Synced files $argv"
-  else
-    notify-send -i /home/scvh/Pictures/icons/error.png "Error in sync"
-  end
-end
--- fish/panel/todochecker.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-set year (date +'%Y')
-set month (date +'%m')
-cd /home/scvh/Notes/
-cd $year
-set filename "note-$year-$month.md"
-echo : (grep '\[ ]' $filename | wc -l)
