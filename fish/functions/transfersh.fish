7b9ac08a634b89d82df4aa15c6cd8c9d1eeccf77
schvabodka-man <scvhapps@gmail.com>
2016-12-06 21:01:58 +0200

    fix in transfer.sh script

--- fish/functions/transfersh.fish
+++ fish/functions/transfersh.fish
@@ -1,8 +1,8 @@
 function transfersh
   set icon '/home/scvh/Pictures/icons/upload.png'
   for file in $argv
-    curl --upload-file $file https://transfer.sh/$file | xclip -selection clipboard
     notify-send -i $icon "Uploading $file"
+    curl --upload-file $file https://transfer.sh/$file | xclip -selection clipboard
   end
   notify-send -i $icon "All files uploaded"
 end
