9c0ce1658ca7c437a5b23907017ea6ad61596db7
schvabodka-man <scvhapps@gmail.com>
2016-12-11 18:02:34 +0200

    updated gog script

--- fish/functions/gog.fish
+++ fish/functions/gog.fish
@@ -1,4 +1,17 @@
 #shortcut for lgogdownloader
 function gog
-  lgogdownloader $argv
+  switch $argv[1]
+  case list
+    lgogdownloader --list
+  case list-detail
+    lgogdownloader --list-detail
+  case install
+    lgogdownloader --download --game $argv[2]
+  case update
+    lgogdownloader --update-check
+  case wishlist
+    lgogdownloader --wishlist
+  case '*'
+    lgogdownloader $argv
+  end
 end
