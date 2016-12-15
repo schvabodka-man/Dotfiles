e090becd0f9e2f694e1755d288492bee717a3947
schvabodka-man <scvhapps@gmail.com>
2016-12-15 08:42:24 +0200

    migrated to slack and ifttt maker

--- fish/notifications/pushbullet_low_battery_notification_laptop.fish
+++ fish/notifications/pushbullet_low_battery_notification_laptop.fish
@@ -5,7 +5,7 @@ if test $battery -gt 15
   echo '0' > /home/scvh/logs/pushbullet_laptop_notification.log #when the battery is charged its set variable to false
 end
 if test $battery -le 15; and test $checkerboolean -eq 0
-  ntfy -c /home/scvh/.config/ntfy/laptop_low_battery_smartphone.yaml -t Laptop send 'battery is low'
+  curl -X POST https://maker.ifttt.com/trigger/laptop_low_battery/with/key/$IFTTT
   echo '1' > /home/scvh/logs/pushbullet_laptop_notification.log #it's set variable to true so push will note be send more than one time
 end
 echo " " #костыль созданный для выпиливания выхлопа
