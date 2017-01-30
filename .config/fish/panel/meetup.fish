3609792a647e26d98298b9f15ff185fba8579a11
schvabodka-man <scvhapps@gmail.com>
2017-01-30 10:20:14 +0200

    nothing interesting just fixed promts

--- .config/fish/panel/meetup.fish
+++ .config/fish/panel/meetup.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/fish
 set date (meetup-cli | grep "Date:" | perl -n -e '/ +Date: (.+)/ && print "$1\n"')
 set counter (meetup-cli | grep "Date:" | wc -l)
-echo  : $counter — $date
+echo   $counter — $date
--- .config/fish/panel/weather.fish
+++ .config/fish/panel/weather.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/fish
 set forecast (forecast "Uzhgorod" | grep '^[. +]' | sed "s/....//")
-echo   $forecast
+echo  $forecast
