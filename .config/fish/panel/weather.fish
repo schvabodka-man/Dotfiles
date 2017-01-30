0a3016b268c41e56d0deeccb55014ee3cbfe3f05
schvabodka-man <scvhapps@gmail.com>
2017-01-30 15:41:33 +0200

    managed to go with polybar

--- .config/fish/panel/weather.fish
+++ .config/fish/panel/weather.fish
@@ -1,3 +1,5 @@
 #!/usr/bin/fish
 set forecast (forecast "Uzhgorod" | grep '^[. +]' | sed "s/....//")
-echo  $forecast
+set celsius (echo $forecast | perl -n -e '/(\d+)/ && print "$1\n"')
+set weather (echo $forecast | perl -n -e '/([a-zA-Z ]+)/ && print "$1\n"')
+echo  $celsius $weather
