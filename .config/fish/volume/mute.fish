f5469832d8374c955b29b18b980dc975924bb709
schvabodka-man <scvhapps@gmail.com>
2017-02-02 14:59:53 +0200

    that's a nice mute script

--- .config/fish/volume/mute.fish
+++ .config/fish/volume/mute.fish
@@ -1,4 +1,11 @@
 #!/usr/bin/fish
-set icon /home/user/Pictures/icons/sound-off.svg
+set iconmuted /home/user/Pictures/icons/sound-off.svg
+set icon /home/user/Pictures/icons/sound.svg
 pactl set-sink-mute 0 toggle
-notify-send -i $icon "MUTED $volume"
+set amixerstdout (amixer get Master | perl -n -e '/\[(\w+)\]/ && print "$1\n"' | head -n 1) #i'm parsing only first line because simply if you have one audio device enabled every else is enabled too
+if [ $amixerstdout = "off" ]
+  notify-send -i $iconmuted "MUTED"
+else
+  set volume (amixer get Master | grep -E -o '[0-9]{1,3}?%' | head -1)
+  notify-send -i $icon "$volume"
+end
