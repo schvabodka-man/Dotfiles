242137f593b61979fb8a545a1f1cae872af05149
schvabodka-man <scvhapps@gmail.com>
2017-02-02 15:03:54 +0200

    timing for scripts

--- .config/fish/volume/decrease_volume.fish
+++ .config/fish/volume/decrease_volume.fish
@@ -2,4 +2,4 @@
 set icon /home/user/Pictures/icons/sound.svg
 pactl set-sink-volume 0 -5% #decrease sound volume
 set volume (amixer get Master | grep -E -o '[0-9]{1,3}?%' | head -1)
-notify-send -i $icon "$volume"
+notify-send -t 1500 -i $icon "$volume"
