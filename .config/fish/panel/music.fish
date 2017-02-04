20b7a18dd3688446a27ce0737a83f96f59b537de
schvabodka-man <scvhapps@gmail.com>
2017-02-04 20:44:05 +0200

    adjust to style

--- .config/fish/panel/music.fish
+++ .config/fish/panel/music.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/fish
-set artist (cmus-remote -Q 2>/dev/null | grep artist | cut -d " " -f 3-)
+set artist (cmus-remote -Q 2>/dev/null | grep artist | cut -d " " -f 3- | head -n 1)
 set track (cmus-remote -Q 2>/dev/null | grep title | cut -d " " -f 3-)
 set number (cmus-remote -Q 2>/dev/null | grep tracknumber | cut -d " " -f 3-)
 echo   $artist — $number. $track
