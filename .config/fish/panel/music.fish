199b8bb2b830e22af9aa3283c66d452e76aa1f70
schvabodka-man <scvhapps@gmail.com>
2017-02-04 15:30:47 +0200

    script for bar with music

---
+++ .config/fish/panel/music.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+set artist (cmus-remote -Q 2>/dev/null | grep artist | cut -d " " -f 3-)
+set track (cmus-remote -Q 2>/dev/null | grep title | cut -d " " -f 3-)
+set number (cmus-remote -Q 2>/dev/null | grep tracknumber | cut -d " " -f 3-)
+echo   $artist — $number. $track
