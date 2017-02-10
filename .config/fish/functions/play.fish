b52f25f5405d9c4c2af1e0ace19a7930ae95faaf
schvabodka-man <scvhapps@gmail.com>
2017-02-10 14:20:59 +0200

    play muh ROMs

---
+++ .config/fish/functions/play.fish
@@ -0,0 +1,8 @@
+function play -d "play this with emulator"
+  set extension (echo "$argv[1]" | perl -n -e '/.+\.(\w+)/ && print "$1\n"') #yes i'm echoing it
+  if [ "$extension" = "md" ]
+    gens-gs $argv[1]
+  else
+    echo "It seems like it's not a ROM"
+  end
+end
