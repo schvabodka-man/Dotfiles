4b9c2f6478eecce7872bec6d67bb8124e6d9717e
schvabodka-man <scvhapps@gmail.com>
2017-03-04 11:16:40 +0200

    snes

--- .config/fish/setup/emulators.fish
+++ .config/fish/setup/emulators.fish
@@ -4,3 +4,5 @@ sudo dnf install gens-gs
 sudo dnf install fceux
 sudo dnf install pcsxr
 sudo dnf install stella
+sudo dnf install snes9x
+sudo dnf install snes9x-gtk
---
+++ Games/Genesis/GameGenie/OutRun (USA, Europe).pat
@@ -0,0 +1,3 @@
+MDP Game Genie Patch Code File
+M68K:0003A8:4E75:Outrun Master Code
+M68K:0035A8:4E75:Outrun infinite time
