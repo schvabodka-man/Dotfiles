5022111a45aba1efc3bbda910ae0f22496fbd2aa
schvabodka-man <scvhapps@gmail.com>
2017-04-13 13:48:16 +0300

    lots of stuff

--- .config/fish/setup/emulators.fish
+++ .config/fish/setup/emulators.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/fish
-sudo dnf install mame
-sudo dnf install gens-gs
-sudo dnf install fceux
-sudo dnf install pcsxr
-sudo dnf install stella
-sudo dnf install snes9x
-sudo dnf install snes9x-gtk
-sudo dnf install lxdream
+sudo dnf install mame #MAME
+sudo dnf install gens-gs #Sega Genesis
+sudo dnf install fceux #NES
+sudo dnf install pcsxr #PS1
+sudo dnf install stella #Atari 2600
+sudo dnf install snes9x #Snes
+sudo dnf install snes9x-gtk #Snes9x frontend
+sudo dnf install meka #Sega master system
+sudo dnf install lxdream #Sega dreamcast
