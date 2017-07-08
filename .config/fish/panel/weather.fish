f7215761dfe1c3eb6aac2c2c80a382b0b9f25494
schvabodka-man <scvhapps@gmail.com>
2017-07-08 11:44:02 +0300

    bugfix

--- .config/fish/panel/weather.fish
+++ .config/fish/panel/weather.fish
@@ -1,4 +1,2 @@
 #!/usr/bin/fish
-set local(docker exec -i weather-npm forecast "Uzhgorod")
-set forecast (echo $local | sed -r "s/\x1B\[[0-9;]*[a-zA-Z]//g" | grep '^[. +]' | sed "s/....//" | sed 's/\.//g' )
-echo  $forecast
+echo  (docker exec -i weather-npm forecast | sed -r "s/\x1B\[[0-9;]*[a-zA-Z]//g" | grep '^[. +]' | sed "s/....//" | sed 's/\.//g')
