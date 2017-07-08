1f664d12890688e4ae6b2f185843001c5b38abc4
schvabodka-man <scvhapps@gmail.com>
2017-07-08 13:59:19 +0300

    tmux

--- .config/fish/setup/docker-init.fish
+++ .config/fish/setup/docker-init.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
-docker run --name tldr --restart always -t scvh.com/tldr
-docker run --name weather-npm --restart always -t scvh.com/weather-npm
-docker run --name meetup-cli --restart always -v $HOME/.config/meetup-cli:/root/ -t scvh.com/meetup-cli
-#docker run --name newsbeuter --restart always -v $HOME/.newsbeuter:/root/.newsbeuter/:z -itd scvh.com/newsbeuter /bin/bash
+docker run --name tldr -t scvh.com/tldr
+docker run --name weather-npm -t scvh.com/weather-npm
+docker run --name meetup-cli -v $HOME/.config/meetup-cli:/root/ -t scvh.com/meetup-cli
+docker run --name newsbeuter -v $HOME/.newsbeuter:/root/.newsbeuter/:z -itd scvh.com/newsbeuter 
 #docker run --name rtorrent -v ~/Downloads:/root/Downloads:z -v ~/Torrents:/root/Torrents:z -v ~/.config/rtorrent:/root/:z -e -itd scvh.com/rtorrent 
 
