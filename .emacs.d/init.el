47ed597c36d180500d5191d685a2aa6c4b491f4e
schvabodka-man <scvhapps@gmail.com>
2017-07-29 22:00:13 +0300

    emacs scala

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,7 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -323,6 +324,18 @@
   (add-to-list 'auto-mode-alist '("\\.bf$" . bfbuilder-mode))
   (add-to-list 'auto-mode-alist '("\\.brainfuck$" . bfbuilder-mode))
   (define-key bfbuilder-mode-map (kbd "<M-f4>") 'bfbuilder-debug))
+;;live preview for web
+(use-package  simple-httpd
+  :ensure t)
+(use-package  impatient-mode
+  :ensure t
+  :after simple-httpd
+  :config (add-hook 'web-mode-hook 'impatient-mode)
+  (add-hook 'html-mode-hook 'impatient-mode))
+;;Scala development
+(use-package ensime
+  :ensure t
+  :pin melpa-stable)
 
 ;;shit from GitHub
 ;;muh books
@@ -378,6 +391,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(httpd-port 8088)
  '(minimap-buffer-name " MINIMAP")
  '(minimap-dedicated-window nil)
  '(minimap-hide-fringes t)
