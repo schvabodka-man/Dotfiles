e837be56b64ae8b0a461b66ec7f3e788d135fa41
schvabodka-man <scvhapps@gmail.com>
2017-10-09 17:49:47 +0300

    cleanup

--- .config/fish/functions/cover.fish
+++
@@ -1,6 +0,0 @@
-function cover
-	for c in (find ~/Music -type f -iname cover.jpg)
-		set coverPath (echo $c | perl -n -e '/(.+)cover/ && print "$1\n"')
-		convert $c -resize 200x200 "$coverPath/cover_small.jpg"
-	end
-end
