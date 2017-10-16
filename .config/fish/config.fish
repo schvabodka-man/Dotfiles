3e285c00f93496866099613eb57efee09b1ee862
schvabodka-man <scvhapps@gmail.com>
2017-10-16 17:49:28 +0300

    gitk

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -259,9 +259,24 @@ function git-remote
 	git remote remove origin
 	git remote add origin $argv
 end
+
 function git-squash
 	git reset --soft HEAD~$argv
 	git commit -m 'Squashed last $argv commits'
+end
+
+function git-smart-gui
+	if count $argv > /dev/null
+		if git branch --list -a | grep $argv > /dev/null
+			gitk $argv
+		else if git ls | grep $argv > /dev/null
+			gitk --all $argv
+		else
+			gitk $argv
+		end
+	else
+		gitk --all
+	end
 end
 
 function zip
